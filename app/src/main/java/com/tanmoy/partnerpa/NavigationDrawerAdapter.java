cd271d1629410b28c68e17f7ae738fe12276869b
Tanmoy <tanmuk60@gmail.com>
2017-04-18 09:12:11 +0530

    Initial commit --> App Built from scratch using the Documentation in that instruction website --> App now has a min API level of 23

---
+++ app/src/main/java/com/tanmoy/partnerpa/NavigationDrawerAdapter.java
@@ -0,0 +1,75 @@
+package com.tanmoy.partnerpa;
+
+/**
+ * Created by Ravi on 29/07/15.
+ */
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+import com.tanmoy.partnerpa.R;
+import com.tanmoy.partnerpa.NavigationDrawerAdapter;
+import com.tanmoy.partnerpa.NavDrawerItem;
+
+import com.tanmoy.partnerpa.NavDrawerItem;
+
+public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDrawerAdapter.MyViewHolder> {
+    List<NavDrawerItem> data = Collections.emptyList();
+    private LayoutInflater inflater;
+    private Context context;
+
+    public NavigationDrawerAdapter(Context context, List<NavDrawerItem> data) {
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+        this.data = data;
+    }
+
+    public void delete(int position) {
+        data.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.nav_drawer_row, parent, false);
+        MyViewHolder holder = new MyViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        NavDrawerItem current = data.get(position);
+        holder.title.setText(current.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
+}
